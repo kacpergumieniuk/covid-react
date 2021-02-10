93d7b70b6e06fd31b57539ece5e7add72baee7c9
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2021-02-10 18:53:01 +0100

    Mobile Main section + animations

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,34 @@
-import React from 'react'
-import Virus1 from './images/virus1.svg'
-import Virus2 from './images/virus2.svg'
+import React, { useRef, useEffect } from 'react'
+import {ReactComponent as Virus1} from './images/virus1.svg'
+import {ReactComponent as Virus2} from './images/virus2.svg'
+import gsap from 'gsap'
 
 function Main() {
+
+    const wrapper = useRef(null);
+
+    useEffect(() => {
+
+        const virus1 = wrapper.current.children[0];
+        const virus2 = wrapper.current.children[1];
+
+        gsap.set([virus1, virus2], {authoAlpha: 0});
+
+        const tl = gsap.timeline({defaults: {ease: 'power3.inOut'}});
+
+        tl.fromTo(virus2, {x: '-=100%'}, {duration: 2, x: '+=100%', autoAlpha: 1})
+          .fromTo(virus1, {x: '+=100%'}, {duration: 2, x: '-=100%', autoAlpha: 1}, '-=1.4')       
+
+    }, [])
+
     return (
         <div className='main'>
             <h1>Covid Tracker</h1>
             <div className='button'>Get Started</div>
-            <img src={Virus1} id="virus1"></img>
-            <img src={Virus2} id="virus2"></img>
+            <div ref={wrapper}>
+            <Virus1 id='virus1' />
+            <Virus2 id='virus2' />
+            </div>
         </div>
     )
 }
